613b8dd91d5aa66b78e0d21acb28499c7e5b9a0d
udehlee <ritzeepita@gmail.com>
2024-03-16 04:16:10 +0100

    initial commit

---
+++ Week-Two/Day-One/Day-Three/main.go
@@ -0,0 +1,45 @@
+package main
+
+import (
+	"html/template"
+	"log"
+	"net/http"
+
+	"github.com/julienschmidt/httprouter"
+)
+
+var tpl *template.Template
+
+func init() {
+	tpl = template.Must(template.ParseGlob("templates/*"))
+}
+
+func main() {
+	mux := httprouter.New()
+	mux.GET("/", index)
+	mux.GET("/about", about)
+	mux.GET("/contact", contact)
+
+}
+
+func index(w http.ResponseWriter, req *http.Request, _ httprouter.Params) {
+	err := tpl.ExecuteTemplate(w, "index.gohtml", nil)
+	HandleError(w, err)
+}
+
+func about(w http.ResponseWriter, req *http.Request, _ httprouter.Params) {
+	err := tpl.ExecuteTemplate(w, "about.gohtml", nil)
+	HandleError(w, err)
+}
+
+func contact(w http.ResponseWriter, req *http.Request, _ httprouter.Params) {
+	err := tpl.ExecuteTemplate(w, "contact.gohtml", nil)
+	HandleError(w, err)
+}
+
+func HandleError(w http.ResponseWriter, err error) {
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		log.Fatalln(err)
+	}
+}
---
+++ Week-Two/Day-One/Day-Three/readme.md
@@ -0,0 +1,3 @@
+## Overview
+
+ I learned how to use  Julien Schmidt's router for routing and rendering HTML templates in Go.
